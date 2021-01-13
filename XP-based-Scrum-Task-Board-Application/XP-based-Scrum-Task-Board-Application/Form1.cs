db53910b0fabcda702e7880fcff5d01b7bb68231
ahmetcetiner <66512698+ahmetcetiner@users.noreply.github.com>
2021-01-13 15:39:04 +0300

    feat: Add c# Project

---
+++ XP-based-Scrum-Task-Board-Application/XP-based-Scrum-Task-Board-Application/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XP_based_Scrum_Task_Board_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ XP-based-Scrum-Task-Board-Application/XP-based-Scrum-Task-Board-Application/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XP_based_Scrum_Task_Board_Application
+{
+    static class Program
+    {
+        /// <summary>
+        /// Uygulamanın ana girdi noktası.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
